fe4cfe8b853a021a3c3053038c2e0df0964d0f62
Fumihito Morita <adringonion.byazura@gmail.com>
2021-05-30 23:39:18 +0900

    JSONを返すように修正

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #[macro_use]
 extern crate rocket;
 use crate::user::User;
+use rocket::http::Status;
 use rocket_contrib::json;
 use rocket_contrib::json::Json;
 use rocket_contrib::json::JsonValue;
@@ -10,7 +11,7 @@ use rocket_contrib::json::JsonValue;
 mod token;
 mod user;
 
-#[post("/create", data = "<new_user>")]
+#[post("/create", data = "<new_user>", format = "json")]
 fn user_create(new_user: Json<User>) -> JsonValue {
     let token = token::Token::new();
     json!({
@@ -18,13 +19,18 @@ fn user_create(new_user: Json<User>) -> JsonValue {
     })
 }
 
-#[get("/get")]
+#[get("/get", format = "json")]
 fn user_get() -> Json<User> {
     Json(User::new("test"))
 }
 
+#[put("/update", data = "<user>", format = "json")]
+fn user_update(user: Json<User>) -> Status {
+    Status::Ok
+}
+
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/user", routes![user_create, user_get])
+    rocket::ignite().mount("/user", routes![user_create, user_get, user_update])
 }
 
 fn main() {
