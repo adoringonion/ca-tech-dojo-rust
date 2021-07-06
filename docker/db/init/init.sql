8e8a9579663ca01809a88b1b8c9ccd909b182408
adoringonion <adringonion.byazura@gmail.com>
2021-07-07 01:29:47 +0900

    SQL修正

--- docker/db/init/init.sql
+++ docker/db/init/init.sql
@@ -43,7 +43,8 @@ CREATE TABLE user_has_character
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     user_id INTEGER NOT NULL,
     character_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL
+    quantity INTEGER NOT NULL,
+    UNIQUE user_character_index (id, user_id, character_id)
 );
 
 INSERT INTO user_has_character (user_id, character_id, quantity) VALUES (1, 1, 3);
